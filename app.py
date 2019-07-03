1f8df8d619bde67f2b03e2a182340f158d56055e
ricaportela <ricaportela@gmail.com>
2019-07-03 20:00:29 -0300

    Update Find by transaction id

--- app.py
+++ app.py
@@ -31,10 +31,31 @@ class Transaction_All(Resource):
         return jsonify({'resultado': output})
 
 
+
+class TransactionById(Resource):
+    def get(self, id):
+        transaction = mongo.db.transactions
+
+        q = transaction.find_one({'id' : id})
+
+        if q:
+            output = {'id' : q['id'], 
+                      'data' : q['data'],
+                      'hora' : q['hora'],
+                      'containicial' : q['containicial'],
+                      'contafinal' : q['contafinal'],
+                      'valor' : q['valor']
+                    }
+        else:
+            output = 'No transaction found'
+
+        return jsonify({'result' : output})
+
+
 class Transaction(Resource):
-    def get(self):
-        return ({'name': "get mensagem"})
-    
+#   def get(self):
+#       return ({'name': "get mensagem"}) 
+
     def post(self):
         return ({'name': "post mensagem"})
     
@@ -44,9 +65,15 @@ class Transaction(Resource):
     def delete(self):
         return ({'name': "delete mensagem"})
 
+class Search(Resource):
+    def post(self):
+        return ({'name': "search dates"})
+
 api = Api(app)
 api.add_resource(Transaction_All, "/Transactions",  endpoint="transactions")
 api.add_resource(Transaction, "/Transaction",  endpoint="transaction")
+api.add_resource(TransactionById, "/TransactionById/<int:id>",  endpoint="transactionbyid")
+api.add_resource(Search, "/Search",  endpoint="search")
 
 
 if __name__ == "__main__":
@@ -58,7 +85,7 @@ if __name__ == "__main__":
 def filter_entry():
     start = parse(request.form['start'])
     end = parse(request.form['end'])
-    cur = mongo.db.user.find({'birthday': {'$lt': end, '$gte': start}})
+    cur = mongo.db.transaction.find({'birthday': {'$lt': end, '$gte': start}})
     results = []
     for row in cur:
         results.append({"name": row['name'], "birthday": row['birthday'].strftime("%Y/%m/%d")})
