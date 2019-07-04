6d5661d6506b94997e09be9c718470449a1b6607
ricaportela <ricaportela@gmail.com>
2019-07-03 21:17:49 -0300

    Update run app Flask

--- app.py
+++ app.py
@@ -77,7 +77,8 @@ api.add_resource(Search, "/Search",  endpoint="search")
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
